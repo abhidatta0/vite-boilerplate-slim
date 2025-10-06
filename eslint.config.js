07e965bd47e08324adbd6bc23e944071ac398ece
abhidatta0 <abhidatta146@gmail.com>
2025-10-06 09:03:42 +0530

    eslint config

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import reactHooks from 'eslint-plugin-react-hooks';
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js,'react-hooks': reactHooks }, extends: ["js/recommended",'react-hooks/recommended'], languageOptions: { globals: globals.browser } },
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {...pluginReact.configs.flat.recommended,
+    settings:{
+      react:{
+        version:'detect'
+      }
+    }
+  },pluginReact.configs.flat['jsx-runtime'],
+  {
+    rules:{
+      'react-proptypes':'off',
+    }
+  }
+]);
