033dc4e133269b94385ddc0bc2593758eefdbc42
abhidatta0 <abhidatta146@gmail.com>
2025-10-06 09:13:24 +0530

    Prettier setup

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import reactHooks from 'eslint-plugin-react-hooks';
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js,'react-hooks': reactHooks }, extends: ["js/recommended",'react-hooks/recommended'], languageOptions: { globals: globals.browser } },
@@ -16,6 +17,7 @@ export default defineConfig([
       }
     }
   },pluginReact.configs.flat['jsx-runtime'],
+  eslintConfigPrettier,
   {
     rules:{
       'react-proptypes':'off',
